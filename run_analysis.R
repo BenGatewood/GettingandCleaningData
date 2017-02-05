27906b4812656becd2215801985945e751bb9de4
Ben Gatewood <ben@gatewood.org.uk>
2017-02-05 21:31:53 +0000

    Added CSV output

--- run_analysis.R
+++ run_analysis.R
@@ -73,5 +73,8 @@ melted_set <- melt(merged_set, id = c("subject", "activity"))
 # Then dcast() the melted set via mean()
 final_tidy <- dcast(melted_set, subject+activity ~ variable, mean)
 
+# Write out the final, tidy dataset as a csv:
+write.csv(final_tidy, "final_tidy.csv", row.names=FALSE)
+
 
 
